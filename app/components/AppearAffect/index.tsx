853d7522c01ffd46b07f1dab6e01f75d9f200464
BeToast <note2bp@protonmail.com>
2023-09-11 19:21:26 +0100

    useEffect() to avoid hydration mismatch

--- app/components/AppearAffect/index.tsx
+++ app/components/AppearAffect/index.tsx
@@ -2,29 +2,40 @@
 
 import Fade from "components/Fade";
 import WidthGrow from "components/WidthGrow";
-
-// import config from '@/tailwind.config'
-// const screens = config.theme?.screens;
+import { useEffect, useState } from "react";
 
 const pc = 956;
+// const client = (typeof window === "undefined" ? false : true);
+
 
 const AppearAffect: React.FC<{
   children: React.ReactNode,
 }> = ({
   children,
 }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return(<>
-  {(window.innerWidth > pc 
-  ?
+  {(isClient ? 
+    (window.innerWidth > pc 
+    ?
+      <WidthGrow>
+        {children}
+      </WidthGrow>
+    :
+      <Fade.In>
+        {children}
+      </Fade.In>
+    )
+  :
     <WidthGrow>
       {children}
     </WidthGrow>
-  :
-    <Fade.In>
-      {children}
-    </Fade.In>
   )}
-    
   </>);
 }
 
