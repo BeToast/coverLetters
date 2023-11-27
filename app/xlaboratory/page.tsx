e3a3a4373ccf938686829c86697d0363329dee2b
BeToast <note2bp@protonmail.com>
2023-11-27 23:13:50 +0000

    xlaboratory

--- app/xlaboratory/page.tsx
+++ app/xlaboratory/page.tsx
@@ -8,81 +8,81 @@ import Signature from 'components/Signature';
 // scroll  handler
 import dynamic from 'next/dynamic'
 import { cvUrl } from '../static';
-const DynamicScrollHandler = dynamic(() => import('components/ScrollHandler'),{
-  ssr: false,
+const DynamicScrollHandler = dynamic(() => import('components/ScrollHandler'), {
+   ssr: false,
 });
 
 export default function Template() {
-  const mainId = "c3ai";
+   const mainId = "xlaboratory";
 
-  return (<>
-    <main id={mainId} className='flex flex-wrap justify-center items-center min-h-full w-[940px] max-w-full m-auto'>
-      <div className={'px-10 pt-12 pc:pt-0 pc:pl-10 pc:pr-0 pc:h-[640px] w-full pc:w-[540px] pc:order-last'}>
-        <AppearAffect>
-          <div className='flex flex-col justify-center h-full'>
-            <div className='pc:w-[500px]'>
-              <Title>
-              Meet your new developer 😉
-              </Title>
-              <Hr className='my-6'/>
-              <Text.Frame>
-                <Text.Greeting>
-                  Hello C3!
-                </Text.Greeting>
-                <Text.P>
-                  I, Blake Preston, recently finished my studies in Computer Science and Information Technology at the University of Galway. Over the summer, I applied my knowledge and learned how to develop full-stack web apps with Next.js, React, Tailwindcss and Firebase Hosting. I reccently finished a freelance project as a web developer for Soma Co-working Moycullen. <Text.Link noP={true} href='https://somacowork.ie/'>https://somacowork.ie/</Text.Link> Currenly, I am working on a church website template which statically generates routes and pages from markdown files using Next.js, Tailwindcss, and Google Firebase.
-                </Text.P>
-                <Text.P>
-                  I am sure you will be pleased with my technical and my interpersonal skills for this entry-level position. I hope to contribute to the progrssion of AI at the enterprize level with you at C3ai.
-                </Text.P>
-              </Text.Frame>
-            </div>
-            <div className='flex flex-wrap pc:w-[500px] items-end justify-between pt-12 pb-1'>
-              <div className='pr-8'>
-                <Text.Frame>
-                <Text.Link
-                    href={cvUrl}
-                  >
-                    view my CV
-                  </Text.Link>
-                  <Text.Link
-                    href='https://github.com/BeToast'
-                  >
-                    view my Github
-                  </Text.Link>  
-                  <Text.Link
-                    href='https://www.linkedin.com/in/blake-m-preston/'
-                  >
-                    view my Linkedin
-                  </Text.Link>
-                  <Text.Link
-                    href='tel:+353-85-260-9548'
-                  >
-                    085-260-9548
-                  </Text.Link>
-                  <Text.Link
-                    href='mailto:blake.m.preston@gmail.com'
-                  >
-                    blake.m.preston@gmail.com
-                  </Text.Link>
-                </Text.Frame>
-              </div>
-              <div className='mt-12'>
-                <Signature>
-                  I look forward to your reply,
-                </Signature>
-              </div>
-            </div>
-          </div>
-        </AppearAffect>
-      </div>
+   return (<>
+      <main id={mainId} className='flex flex-wrap justify-center items-center min-h-full w-[940px] max-w-full m-auto'>
+         <div className={'px-10 pt-12 pc:pt-0 pc:pl-10 pc:pr-0 pc:h-[640px] w-full pc:w-[540px] pc:order-last'}>
+            <AppearAffect>
+               <div className='flex flex-col justify-center h-full'>
+                  <div className='pc:w-[500px]'>
+                     <Title>
+                        Meet your new Full Stack Software Developer 😉
+                     </Title>
+                     <Hr className='my-6' />
+                     <Text.Frame>
+                        <Text.Greeting>
+                           Hello C3!
+                        </Text.Greeting>
+                        <Text.P>
+                           I, Blake Preston, recently finished my studies in Computer Science and Information Technology at the University of Galway. Over the summer, I applied my knowledge and learned how to develop full-stack web apps with Next.js, React, Tailwindcss and Firebase Hosting. I reccently finished a freelance project as a web developer for Soma Co-working Moycullen. <Text.Link noP={true} href='https://somacowork.ie/'>https://somacowork.ie/</Text.Link> Currenly, I am working on a church website template which statically generates routes and pages from markdown files using Next.js, Tailwindcss, and Google Firebase.
+                        </Text.P>
+                        <Text.P>
+                           I am sure you will be pleased with my technical and my interpersonal skills for this entry-level position. I hope to contribute to the progrssion of AI at the enterprize level with you at C3ai.
+                        </Text.P>
+                     </Text.Frame>
+                  </div>
+                  <div className='flex flex-wrap pc:w-[500px] items-end justify-between pt-12 pb-1'>
+                     <div className='pr-8'>
+                        <Text.Frame>
+                           <Text.Link
+                              href={cvUrl}
+                           >
+                              view my CV
+                           </Text.Link>
+                           <Text.Link
+                              href='https://github.com/BeToast'
+                           >
+                              view my Github
+                           </Text.Link>
+                           <Text.Link
+                              href='https://www.linkedin.com/in/blake-m-preston/'
+                           >
+                              view my Linkedin
+                           </Text.Link>
+                           <Text.Link
+                              href='tel:+353-85-260-9548'
+                           >
+                              085-260-9548
+                           </Text.Link>
+                           <Text.Link
+                              href='mailto:blake.m.preston@gmail.com'
+                           >
+                              blake.m.preston@gmail.com
+                           </Text.Link>
+                        </Text.Frame>
+                     </div>
+                     <div className='mt-12'>
+                        <Signature>
+                           I look forward to your reply,
+                        </Signature>
+                     </div>
+                  </div>
+               </div>
+            </AppearAffect>
+         </div>
 
-      <div className='mt-16 pc:mt-0 w-[400px] '>
-        <Photo />
-      </div>
-    </main>
-    
+         <div className='mt-16 pc:mt-0 w-[400px] '>
+            <Photo />
+         </div>
+      </main>
 
-    <DynamicScrollHandler mainId={mainId}/>
-  </>)
+
+      <DynamicScrollHandler mainId={mainId} />
+   </>)
 }
