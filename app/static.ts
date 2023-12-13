14b58bc5b62e7edc77f37d80f4f3739423919b14
BeToast <note2bp@protonmail.com>
2023-12-13 14:31:13 +0000

    peraton

--- app/static.ts
+++ app/static.ts
@@ -1,4 +1,4 @@
-const cvUrl = 'https://drive.google.com/file/d/1ZQ22k7DEG-iPaY0FqJUnwqBLBrBCpds2/view?usp=sharing' ;
+const cvUrl = 'https://drive.google.com/file/d/1m3LY8nRPMlAZaedFrOdP0OzQAehKqSrU/view?usp=sharing' ;
 const cvUrlHospitality = 'https://drive.google.com/file/d/1snpOpAcZ5YFG6nLZoQDX740ZjIiY9F72/view?usp=sharing' ;
 const githubUrl = 'https://github.com/BeToast'
 
