d6757f701a47b68a2404a9482b659a8f83394f48
Blake Preston <note2bp@protonmail.com>
2024-01-22 12:29:00 -0800

    AION

---
+++ app/AION/page.tsx
@@ -0,0 +1,88 @@
+import Photo from 'components/Photo'
+import Title from 'components/Title'
+import Text from 'components/Text'
+import AppearAffect from 'components/AppearAffect';
+import Hr from 'components/Hr';
+import Signature from 'components/Signature';
+
+// scroll  handler
+import dynamic from 'next/dynamic'
+import { cvUrl } from '../static';
+const DynamicScrollHandler = dynamic(() => import('components/ScrollHandler'), {
+   ssr: false,
+});
+
+export default function Template() {
+   const mainId = "sonarski";
+
+   return (<>
+      <main id={mainId} className='flex flex-wrap justify-center items-center min-h-full w-[940px] max-w-full m-auto'>
+         <div className={'px-10 pt-12 pc:pt-0 pc:pl-10 pc:pr-0 pc:h-[680px] w-full pc:w-[540px] pc:order-last'}>
+            <AppearAffect>
+               <div className='flex flex-col justify-center h-full'>
+                  <div className='pc:w-[500px]'>
+                     <Title>
+                        Looking for a high-potential Computer Science Graduate? 🤔
+                     </Title>
+                     <Hr className='my-6' />
+                     <Text.Frame>
+                        <Text.Greeting>
+                           Greetings AION,
+                        </Text.Greeting>
+                        <Text.P>
+                           I, Blake Preston, recently finished my studies in Computer Science and Information Technology at the University of Galway. Over the summer, I applied my knowledge and learned how to develop full-stack web apps with Next.js, React, Tailwindcss and Firebase Hosting. I reccently finished a freelance project as a web developer for Soma Co-working Moycullen. <Text.Link noP={true} href='https://somacowork.ie/'>https://somacowork.ie/</Text.Link>
+                        </Text.P>
+                        <Text.P>
+                           I want to begin my career at an innovative company like AION. I am best suited as a web developer, however, I have also had sucess with deep neural networks for NLP. Due to my adaptablity, I would be able to contribute to various software projects to fufill your buisness opjectives.
+                        </Text.P>
+                     </Text.Frame>
+                  </div>
+                  <div className='flex flex-wrap pc:w-[500px] items-end justify-between pt-12 pb-1'>
+                     <div className='pr-8'>
+                        <Text.Frame>
+                           <Text.Link
+                              href={cvUrl}
+                           >
+                              view my CV
+                           </Text.Link>
+                           <Text.Link
+                              href='https://github.com/BeToast'
+                           >
+                              view my Github
+                           </Text.Link>
+                           <Text.Link
+                              href='https://www.linkedin.com/in/blake-m-preston/'
+                           >
+                              view my Linkedin
+                           </Text.Link>
+                           <Text.Link
+                              href='tel:+353-85-260-9548'
+                           >
+                              085-260-9548
+                           </Text.Link>
+                           <Text.Link
+                              href='mailto:blake.m.preston@gmail.com'
+                           >
+                              blake.m.preston@gmail.com
+                           </Text.Link>
+                        </Text.Frame>
+                     </div>
+                     <div className='mt-12'>
+                        <Signature>
+                           I hope to start a career with you,
+                        </Signature>
+                     </div>   
+                  </div>
+               </div>
+            </AppearAffect>
+         </div>
+
+         <div className='mt-16 pc:mt-0 w-[400px] '>
+            <Photo />
+         </div>
+      </main>
+
+
+      <DynamicScrollHandler mainId={mainId} />
+   </>)
+}
